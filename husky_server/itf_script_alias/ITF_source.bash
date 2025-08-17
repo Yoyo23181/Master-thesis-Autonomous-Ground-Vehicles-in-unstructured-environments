7a7d1b083f2cf40053e06f9fc4f49b8109bd0cc1
Yoyo23181 <57369895+Yoyo23181@users.noreply.github.com>
2025-08-17 18:41:08 +0200

    Update ITF_source.bash

--- husky_server/itf_script_alias/ITF_source.bash
+++ husky_server/itf_script_alias/ITF_source.bash
@@ -1,21 +1,13 @@
-alias itf_git_update=/home/administrator/colcon_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/git_scripts/update_git.sh
-#alias test=/ros_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/git_scripts/test.sh #only in simulation
-alias computer_rviz=/ros_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/husky_sim_docker/husky_sim_docker.sh
-#alias navigation=/home/administrator/colcon_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/husky_launch/husky_launch.sh # for real
-alias navigation=/ros_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/husky_launch/husky_launch.sh # for sim
-#alias husky_server=/home/administrator/colcon_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/husky_launch/husky_server.sh # for real
-alias husky_server=/ros_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/husky_launch/husky_server.sh # for sim
-
 alias proximity=/ros_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/proximity_loc/proximity/proximity.sh # for proximity
 alias rssi=/ros_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/proximity_loc/RSSI/RSSI.sh # for RSSI
 alias slam=/ros_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/proximity_loc/SLAM/slam.sh # for SLAM
 
 alias aaa=/ros_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/Build/build_ros_ws.sh
 
-alias simulation=/ros_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/husky_launch/husky_launch_simu.sh
+
 alias slimulation=/ros_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/husky_launch/husky_launch_simu_slam.sh
 
-alias exp_slam=/ros_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/proximity_loc/SLAM/slam_experiment.sh # for SLAM in experiment
+alias exp_slam=/ros_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/proximity_loc/SLAM/slam_experiment.sh 
 
 
 
