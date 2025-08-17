f1fa13d7e9f0a65456ee2b8023e69cfb97784618
Yoyo23181 <57369895+Yoyo23181@users.noreply.github.com>
2025-08-17 18:27:35 +0200

    Update real_algo.sh

--- tb4_code/script_alias/real_algo.sh
+++ tb4_code/script_alias/real_algo.sh
@@ -7,7 +7,7 @@ cd "$SCRIPTPATH"
 
 echo $SCRIPTPATH
 
-tmuxinator start -p /ros_ws/src/tb4_code/script_alias/real_algo/real_algo.yaml
+tmuxinator start -p /ros_ws/src/tb4_code/script_alias/real_algo.yaml
 
 shopt -s expand_aliases
 source ~/.bashrc
