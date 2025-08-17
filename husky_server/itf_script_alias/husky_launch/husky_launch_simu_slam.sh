73cca000bb02bb42b8b9d0b00d79f6bcaad31267
Yoyo23181 <57369895+Yoyo23181@users.noreply.github.com>
2025-08-17 18:39:57 +0200

    Add files via upload

---
+++ husky_server/itf_script_alias/husky_launch/husky_launch_simu_slam.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -i
+
+SCRIPT=$(readlink -f $0)
+
+SCRIPTPATH=`dirname $SCRIPT`
+cd "$SCRIPTPATH"
+
+echo $SCRIPTPATH
+
+tmuxinator start -p /ros_ws/src/ITF_husky_demo/ITF_ros2_package/itf_script_alias/husky_launch/husky_launch_simu_slam.yaml
+
+shopt -s expand_aliases
+source ~/.bashrc
