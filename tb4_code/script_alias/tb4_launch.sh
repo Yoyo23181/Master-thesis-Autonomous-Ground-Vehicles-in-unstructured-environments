9c253c2fb84636488a318ac84147a87c75599967
Yoyo23181 <57369895+Yoyo23181@users.noreply.github.com>
2025-08-17 18:27:58 +0200

    Update tb4_launch.sh

--- tb4_code/script_alias/tb4_launch.sh
+++ tb4_code/script_alias/tb4_launch.sh
@@ -7,7 +7,7 @@ cd "$SCRIPTPATH"
 
 echo $SCRIPTPATH
 
-tmuxinator start -p /ros_ws/src/tb4_code/script_alias/simulation_launch/tb4_launch.yaml
+tmuxinator start -p /ros_ws/src/tb4_code/script_alias/tb4_launch.yaml
 
 shopt -s expand_aliases
 source ~/.bashrc
