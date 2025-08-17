d84f6fdeb56df1bf9dfab1e4938de11ccbd12122
Yoyo23181 <57369895+Yoyo23181@users.noreply.github.com>
2025-08-17 18:28:12 +0200

    Update tb4_localization_rviz_only.sh

--- tb4_code/script_alias/tb4_localization_rviz_only.sh
+++ tb4_code/script_alias/tb4_localization_rviz_only.sh
@@ -7,7 +7,7 @@ cd "$SCRIPTPATH"
 
 echo $SCRIPTPATH
 
-tmuxinator start -p /ros_ws/src/tb4_code/script_alias/simulation_launch/tb4_localization_rviz_only.yaml
+tmuxinator start -p /ros_ws/src/tb4_code/script_alias/tb4_localization_rviz_only.yaml
 
 shopt -s expand_aliases
 source ~/.bashrc
